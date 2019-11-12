af79804291f7792c17f056e7e3d3dfe85f7a643f
ruben lopez <socoruben@gmail.com>
2019-11-12 12:30:09 +0100

    registro hecho

--- 100M/100M/Inicio.vb
+++ 100M/100M/Inicio.vb
@@ -1,9 +1,16 @@
 ﻿Imports B_C_100M
 Public Class Inicio
     Private Sub BtnAceptar_Click(sender As Object, e As EventArgs) Handles BtnAceptar.Click
-        FormMain.nombreUsuario = TxBNombre.Text
-        FormMain.Show()
-        Me.Hide()
+        Dim file As New Ficheros
+
+        If file.verifUser(TxBNombre.Text, TxBPassword.Text) Then
+            FormMain.nombreUsuario = TxBNombre.Text
+            FormMain.Show()
+            Me.Hide()
+        Else
+            MsgBox("Nombre contraseña incorrectos, por favor vuelva a intentarlo.", 64, "Fallo de inicio de sesión")
+        End If
+
     End Sub
 
     Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
@@ -12,12 +19,10 @@ Public Class Inicio
             Try
                 Application.Exit()
             Catch ex As Exception
-                MsgBox("No se ha podido salir, intentelo denuevo.", MsgBoxStyle.Critical)
+                MsgBox("No se ha podido salir, intentelo de nuevo.", MsgBoxStyle.Critical)
             End Try
         End If
     End Sub
 
-    Private Sub Inicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
 End Class
